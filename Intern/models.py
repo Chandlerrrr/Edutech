df9e475481d98bb4fcdd3b4822ab95d615bcf6c7
gautam khattri <gautamkhattri07@gmail.com>
2020-11-09 16:06:23 +0530

    models

--- Intern/models.py
+++ Intern/models.py
@@ -6,8 +6,8 @@ from django.urls import reverse
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=200)
-    Body = models.TextField()
+    title = models.CharField(max_length=400)
+    Body = models.TextField(max_length=1000)
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
